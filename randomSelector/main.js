3c2939c916baae5729cd5c4c5e6c262cf6f6369b
gavin870121 <83258536+gavin870121@users.noreply.github.com>
2021-05-06 10:13:57 +0800

    0506 1013

--- randomSelector/main.js
+++ randomSelector/main.js
@@ -8,6 +8,6 @@ $(function(){
         $("h1").text($("#choices li").eq(randomChildNumber).text());
         //找到img元件 V
         //設定img元件的src屬性 -> 對應元件的title值
-        //$("img").attr("src",$("li").eq(randomChildNumber).attr("title"));
+        $("#foodImg").attr("src",$("li").eq(randomChildNumber).attr("title"));
     });
 });
